40fbebbeb1f27030f8e95eb61704bbf93cdf11e6
2squarehoon <a01038517287@gmail.com>
2022-12-05 16:35:01 +0900

    '10989'

--- Baekjun/bronze/10989.py
+++ Baekjun/bronze/10989.py
@@ -3,9 +3,19 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-lst = []
+# lst = []
+# for _ in range(N):
+#     lst.append(input().split()[0])
+# lst.sort()
+# for number in lst:
+#     print(number)
+# 시간초과
+
+# 계수정렬
+arr = [0]*10000
 for _ in range(N):
-    lst.append(input().split()[0])
-lst.sort()
-for number in lst:
-    print(number)
+    arr[int(input())-1] += 1
+for i in range(10000):
+    if arr[i]:
+        for j in range(arr[i]):
+            print(i+1)
