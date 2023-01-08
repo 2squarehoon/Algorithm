fb2e27f4297f15074a0415581d623c3b9dbc9d67
SH <shhh1006@daum.net>
2023-01-08 22:46:29 +0900

    10815

---
+++ Baekjun/silver/10815.py
@@ -0,0 +1,14 @@
+# S5 숫자 카드
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+cards = set(map(int, input().split()))
+M = int(input())
+numbers = list(map(int, input().split()))
+for n in numbers:
+    if n in cards:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
