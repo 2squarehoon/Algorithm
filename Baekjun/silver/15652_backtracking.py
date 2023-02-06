0d8ad1cd77d285ac53b2b031b86c602606330c68
SH <shhh1006@daum.net>
2023-02-06 20:30:39 +0900

    15652

---
+++ Baekjun/silver/15652_backtracking.py
@@ -0,0 +1,12 @@
+# S3 N과 M
+N, M = map(int, input().split())
+lst = []
+def dfs(start):
+    if len(lst)==M:
+        print(' '.join(map(str, lst)))
+        return
+    for i in range(start, N+1):
+        lst.append(i)
+        dfs(i)
+        lst.pop()
+dfs(1)
