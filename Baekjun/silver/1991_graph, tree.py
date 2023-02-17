be58a3978421d67a1c58316e1c32969223db2560
SH <shhh1006@daum.net>
2023-02-17 15:29:16 +0900

    1991

---
+++ Baekjun/silver/1991_graph, tree.py
@@ -0,0 +1,31 @@
+# S1 트리 순회
+import sys
+input = sys.stdin.readline
+N = int(input())
+tree = {}
+for _ in range(N):
+    root, left, right = map(str, input().split())
+    tree[root] = [left, right]
+
+def preorder(root):
+    if root != '.':
+        print(root, end='') # root
+        preorder(tree[root][0]) # left
+        preorder(tree[root][1]) # right
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0]) # left
+        print(root, end='') # root
+        inorder(tree[root][1]) # right
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0]) # left
+        postorder(tree[root][1]) # right
+        print(root, end='') # root
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
